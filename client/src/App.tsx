6933e6ff466da8022ff52f5cd3b75ac7d5b02152
sounmind <devleesche@gmail.com>
2024-11-12 01:33:36 -0500

    feat: ModeToggle

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,7 @@
 import AddItem from "@/components/add-item/add-item";
+import { ModeToggle } from "@/components/mode-toogle";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Separator } from "@/components/ui/separator";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ItemList } from "./components/item-list/item-list";
 
@@ -6,10 +9,25 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Review Notes</h1>
-      <AddItem />
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <div className="flex items-start justify-between h-12 mb-2">
+          <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl page-title">
+            Review Notes
+          </h1>
 
-      <ItemList />
+          <div className="self-center">
+            <ModeToggle />
+          </div>
+        </div>
+
+        <Separator className="my-4" />
+
+        <AddItem />
+
+        <Separator className="my-4" />
+
+        <ItemList />
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
