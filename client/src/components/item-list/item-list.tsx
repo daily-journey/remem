0374bb2cd988eb58a4e19d942c128bf7b5ca230a
sounmind <devleesche@gmail.com>
2024-11-12 01:34:51 -0500

    feat: basic item list and item drawer

--- client/src/components/item-list/item-list.tsx
+++ client/src/components/item-list/item-list.tsx
@@ -1,14 +1,17 @@
 import { apiClient } from "@/api-client";
+import ItemDetail from "@/components/item-detail/item-detail";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { useQuery } from "@tanstack/react-query";
 
 export const ItemList = () => {
@@ -17,33 +20,83 @@ export const ItemList = () => {
     queryFn: apiClient.getItems,
   });
 
+  const sorted = items?.toSorted((a, b) => {
+    const aNextReviewDate = new Date(a.reviewDates[0]);
+    const bNextReviewDate = new Date(b.reviewDates[0]);
+
+    return aNextReviewDate.getTime() - bNextReviewDate.getTime();
+  });
+
+  const randomColor = (): { backgroundColor: string; color: string } => {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    const textColor =
+      parseInt(randomColor, 16) > 0xffffff / 2 ? "black" : "white";
+
+    return {
+      backgroundColor: `#${randomColor}`,
+      color: textColor,
+    };
+  };
+
   return (
     <section>
       {areItemsLoading && <p>Loading...</p>}
 
-      <ul>
-        {items?.map((item) => {
-          const dates = item.reviewDates.map((date) => new Date(date));
+      <ul className="flex flex-wrap justify-between gap-2">
+        {sorted?.map((item) => {
+          const nextReviewDate = new Date(item.reviewDates[0])
+            .toISOString()
+            .split("T")[0];
+
           return (
-            <li key={item.id}>
-              <Card className="w-[350px]">
-                <CardHeader>
-                  <CardTitle>
-                    {item.id}. {item.mainText}
-                  </CardTitle>
-                  <CardDescription>{item.subText}</CardDescription>
-                </CardHeader>
+            <Drawer key={item.id}>
+              <DrawerTrigger className="flex-grow w-full text-left md:w-auto hover:cursor-pointer">
+                <li>
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>{item.mainText}</CardTitle>
+                    </CardHeader>
 
-                <CardContent>
-                  <Calendar selected={dates} />
-                </CardContent>
+                    <CardContent>
+                      {item.subText} ... and next review at{" "}
+                      <Badge style={randomColor()}>{nextReviewDate}</Badge>
+                    </CardContent>
+                  </Card>
+                </li>
+              </DrawerTrigger>
 
-                <CardFooter className="flex justify-between">
-                  <Button>Edit</Button>
-                  <Button variant="destructive">Delete</Button>
-                </CardFooter>
-              </Card>
-            </li>
+              <DrawerContent>
+                <div className="w-full max-w-sm mx-auto">
+                  <DrawerHeader>
+                    <DrawerTitle asChild>
+                      <h2 className="pb-2 text-3xl font-semibold tracking-tight border-b scroll-m-20 first:mt-0">
+                        {item.mainText}{" "}
+                        <span className="text-gray-400">(#{item.id})</span>
+                      </h2>
+                    </DrawerTitle>
+
+                    <DrawerDescription>
+                      <div className="mb-2">
+                        <p className="leading-7 [&:not(:first-child)]:mt-6">
+                          {item.subText}
+                        </p>
+                        <p className="leading-7 [&:not(:first-child)]:mt-6">
+                          Next review at{" "}
+                          <Badge style={randomColor()}>{nextReviewDate}</Badge>
+                        </p>
+                      </div>
+
+                      <ItemDetail data={item} />
+                    </DrawerDescription>
+                  </DrawerHeader>
+
+                  <DrawerFooter>
+                    <Button>Edit</Button>
+                    <Button variant="destructive">Delete</Button>
+                  </DrawerFooter>
+                </div>
+              </DrawerContent>
+            </Drawer>
           );
         })}
       </ul>
