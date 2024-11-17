3ba02f0dd17ce105248140a9aed348013c86d6d8
pmjuu <mjuup@Minjuui-MacBookAir.local>
2024-11-17 17:44:26 -0500

    feat: add reviewDatetimes when add new item

--- server/src/main/kotlin/com/laev/reminder/service/ItemService.kt
+++ server/src/main/kotlin/com/laev/reminder/service/ItemService.kt
@@ -3,11 +3,14 @@ package com.laev.reminder.service
 import com.laev.reminder.dto.AddItemRequest
 import com.laev.reminder.entity.Item
 import com.laev.reminder.entity.Member
+import com.laev.reminder.entity.ReviewDatetime
 import com.laev.reminder.exception.ItemCreationException
 import com.laev.reminder.repository.ItemRepository
+import com.laev.reminder.repository.ReviewDatetimeRepository
 import com.laev.reminder.utils.CycleCalculator
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
 import java.time.temporal.ChronoUnit
@@ -15,9 +18,11 @@ import java.time.temporal.ChronoUnit
 @Service
 class ItemService(
     private val itemRepository: ItemRepository,
+    private val reviewDatetimeRepository: ReviewDatetimeRepository,
 ) {
     fun getItems(): List<Item> = itemRepository.findAll()
 
+    @Transactional
     fun addItem(request: AddItemRequest) {
         try {
             val member = Member(1, "Lyla") // TODO
@@ -25,7 +30,11 @@ class ItemService(
             val cycles = listOf(1, 3, 7, 21)
             val reviewDates = CycleCalculator.getReviewDates(createDatetime, cycles)
 
-            itemRepository.save(
+            val offsetHours = request.offset.totalSeconds / 3600 // Convert the offset to hours
+            val zoneOffset = ZoneOffset.ofHours(offsetHours)
+            val zonedCreatedDatetime = createDatetime.withOffsetSameInstant(zoneOffset) // Convert to local time with the applied zoneOffset
+
+            val newItem = itemRepository.save(
                 Item(
                     mainText = request.mainText,
                     subText = request.subText,
@@ -33,6 +42,19 @@ class ItemService(
                     reviewDates = reviewDates.toString(),
                 )
             )
+
+            for (cycle in cycles) {
+                val startDatetime = CycleCalculator.getUTCStartDatetime(zonedCreatedDatetime, cycle, zoneOffset)
+                val endDatetime = startDatetime.plusHours(24)
+
+                reviewDatetimeRepository.save(
+                    ReviewDatetime(
+                        start = startDatetime,
+                        end = endDatetime,
+                        item = newItem,
+                    )
+                )
+            }
         } catch (e: DataIntegrityViolationException) {
             throw ItemCreationException("Failed to create item due to data integrity violation.")
         } catch (e: Exception) {
