3b608b7fc46c78c23c7a69979282cd22691d411b
sounmind <devleesche@gmail.com>
2024-11-17 18:13:12 -0500

    feat: update structure and add error status
    - delete ItemDetail
    - change item-list to review-items
    - add error toast when api call failed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { Toaster } from "@/components/ui/toaster.tsx";
 
 import App from "./App.tsx";
 
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <App />
+        <Toaster />
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
