04a3e1944373b7e8a7f205e71ca23cc29d182349
pmjuu <mjuup@Minjuui-MacBookAir.local>
2024-11-17 16:25:04 -0500

    feat: add ReviewDatetime entity

---
+++ server/src/main/kotlin/com/laev/reminder/entity/ReviewDatetime.kt
@@ -0,0 +1,21 @@
+package com.laev.reminder.entity
+
+import jakarta.persistence.*
+import java.time.OffsetDateTime
+
+@Entity
+class ReviewDatetime(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null,
+
+    @Column(nullable = false)
+    val start: OffsetDateTime,
+
+    @Column(nullable = false)
+    val end: OffsetDateTime,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    val item: Item,
+)
