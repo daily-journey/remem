5f5dba14a62380f9acafba948d8b3a90265339b3
sounmind <devleesche@gmail.com>
2024-11-17 17:06:01 -0500

    feat: apply tabs and api changes

---
+++ client/src/lib/date.ts
@@ -0,0 +1,27 @@
+export function getDatetimeWithOffset() {
+  const date = new Date();
+  const tzo = -date.getTimezoneOffset(),
+    dif = tzo >= 0 ? "+" : "-",
+    pad = function (num: number) {
+      const norm = Math.abs(Math.floor(num));
+      return (norm < 10 ? "0" : "") + norm;
+    };
+
+  return (
+    date.getFullYear() +
+    "-" +
+    pad(date.getMonth() + 1) +
+    "-" +
+    pad(date.getDate()) +
+    "T" +
+    pad(date.getHours()) +
+    ":" +
+    pad(date.getMinutes()) +
+    ":" +
+    pad(date.getSeconds()) +
+    dif +
+    pad(tzo / 60) +
+    ":" +
+    pad(tzo % 60)
+  );
+}
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,3 +1,4 @@
+/** Shadcn/ui config */
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
