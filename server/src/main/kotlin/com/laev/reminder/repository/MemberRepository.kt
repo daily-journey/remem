5ccb07a44ec720b080f30c8c31a8f36588d5ca2f
pmjuu <mjuup@Minjuui-MacBookAir.local>
2024-11-20 23:18:52 -0500

    test: integration test on GET /items and Post /items - use MySQL test_db instead of h2 - remove unit controller test

---
+++ server/src/main/kotlin/com/laev/reminder/repository/MemberRepository.kt
@@ -0,0 +1,8 @@
+package com.laev.reminder.repository
+
+import com.laev.reminder.entity.Member
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface MemberRepository: JpaRepository<Member, Long>
