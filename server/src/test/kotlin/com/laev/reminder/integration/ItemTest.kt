b2c26bc5bea7a866235bd9272ed34e4e717b4037
pmjuu <mjuup@Minjuui-MacBookAir.local>
2024-11-26 17:55:25 -0500

    test: POST /auth/sign-in and add authorization in request header

--- server/src/test/kotlin/com/laev/reminder/integration/ItemTest.kt
+++ server/src/test/kotlin/com/laev/reminder/integration/ItemTest.kt
@@ -2,9 +2,7 @@ package com.laev.reminder.integration
 
 import com.laev.reminder.dto.AddItemRequest
 import com.laev.reminder.entity.Item
-import com.laev.reminder.entity.Member
 import com.laev.reminder.repository.ItemRepository
-import com.laev.reminder.repository.MemberRepository
 import com.laev.reminder.utils.ObjectMapperUtil
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -12,26 +10,20 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.test.context.TestPropertySource
-import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import java.time.ZoneOffset
 
 @SpringBootTest
-@TestPropertySource(locations = ["classpath:application-test.yml"])
 @AutoConfigureMockMvc
 class ItemTest(
-    @Autowired private val mockMvc: MockMvc,
-    @Autowired private val memberRepository: MemberRepository,
     @Autowired private val itemRepository: ItemRepository,
-) {
+) : BaseIntegrationTest() {
     private val objectMapper = ObjectMapperUtil.createObjectMapper()
 
     @BeforeEach
     fun setUp() {
-        val testMember = Member(email = "test@example.com", password = "0000", name = "Lyla")
-        memberRepository.save(testMember)
+        val testMember = createOrGetMember("test@example.com")
         itemRepository.save(
             Item(
                 mainText = "setup item",
@@ -50,16 +42,23 @@ class ItemTest(
             offset = ZoneOffset.of("-05:00"),
         )
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/items")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(request)))
-            .andExpect(MockMvcResultMatchers.status().isCreated)
+        mockMvc.perform(
+            withAuth(
+                MockMvcRequestBuilders.post("/items")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request))
+            )
+        ).andExpect(MockMvcResultMatchers.status().isCreated)
     }
 
     @Test
     fun `fetch items from database`() {
-        mockMvc.perform(MockMvcRequestBuilders.get("/items")
-            .accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(
+            withAuth(
+                MockMvcRequestBuilders.get("/items")
+                    .accept(MediaType.APPLICATION_JSON)
+            )
+        )
             .andExpect(MockMvcResultMatchers.status().isOk)
             .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
             // Check the type and presence of each field
