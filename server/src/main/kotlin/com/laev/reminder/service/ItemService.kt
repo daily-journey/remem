4a183b68f4268097ff6ebfa3c276825cbd88f4a1
pmjuu <mjuup@Minjuui-MacBookAir.local>
2024-11-26 17:59:57 -0500

    feat: extract member from token during POST /items

--- server/src/main/kotlin/com/laev/reminder/service/ItemService.kt
+++ server/src/main/kotlin/com/laev/reminder/service/ItemService.kt
@@ -40,9 +40,8 @@ class ItemService(
     }
 
     @Transactional
-    fun addItem(request: AddItemRequest) {
+    fun addItem(request: AddItemRequest, member: Member) {
         try {
-            val member = Member(1, "test@test.com", "0000", "Lyla") // TODO
             val createDatetime = OffsetDateTime.now(ZoneOffset.UTC).truncatedTo(ChronoUnit.SECONDS) // save time in UTC
             val cycles = listOf(1, 3, 7, 21)
             val reviewDates = CycleCalculator.getReviewDates(createDatetime, cycles)
