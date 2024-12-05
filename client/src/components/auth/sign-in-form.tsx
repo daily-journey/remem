a74a8122bdfe483e6daf463eb102a6d36faf1f7b
sounmind <devleesche@gmail.com>
2024-12-04 21:57:57 -0500

    feat: sign up & log in

---
+++ client/src/components/auth/sign-in-form.tsx
@@ -0,0 +1,119 @@
+import { apiClient } from "@/api-client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const userTokenFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, {
+      message: "Email is required",
+    })
+    .email({
+      message: "Email is not valid",
+    }),
+  password: z.string().min(36, {
+    message: "Password should be at least 36 characters",
+  }),
+});
+
+type SignInFormValues = z.infer<typeof userTokenFormSchema>;
+
+interface Props {
+  showSignUpForm: () => void;
+}
+
+export default function SignInForm({ showSignUpForm }: Props) {
+  const { mutate: signIn } = useMutation({
+    mutationFn: async (command: SignInFormValues) =>
+      await apiClient.signIn(command),
+    onSuccess: () => {
+      toast.success("Sign in successfully");
+    },
+    onError: (error) => {
+      console.error(error);
+      toast.error("Failed to sign in");
+    },
+  });
+
+  const form = useForm<SignInFormValues>({
+    resolver: zodResolver(userTokenFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSignUp(values: SignInFormValues) {
+    signIn(values);
+  }
+
+  return (
+    <>
+      <h1 className="mb-4 text-2xl font-bold">Sign In</h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSignUp)}
+          className="w-full space-y-4"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Password" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+
+      <p className="mt-4">
+        Don&apos;t have an account?{" "}
+        <Button
+          variant="link"
+          onClick={showSignUpForm}
+          className="text-blue-500"
+        >
+          Sign up
+        </Button>
+      </p>
+    </>
+  );
+}
