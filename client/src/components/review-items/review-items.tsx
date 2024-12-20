1e0e8a482087e2c77940e4faeb5b0deda8e93449
sounmind <devleesche@gmail.com>
2024-12-20 18:03:27 -0500

    Chore: hover style, empty list text, button text

--- client/src/components/review-items/review-items.tsx
+++ client/src/components/review-items/review-items.tsx
@@ -64,7 +64,7 @@ export default function ReviewItems({ items, isLoading }: Props) {
     },
   });
 
-  const sorted = items?.toSorted((a, b) => {
+  const sortedItems = items?.toSorted((a, b) => {
     const aNextReviewDate = new Date(a.reviewDates[0]);
     const bNextReviewDate = new Date(b.reviewDates[0]);
 
@@ -108,87 +108,85 @@ export default function ReviewItems({ items, isLoading }: Props) {
   return (
     <section>
       <ul className="flex flex-wrap justify-between gap-2 ">
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          sorted?.map((item) => {
-            const reviewDates = item.reviewDates.map((date) => new Date(date));
-            const nextReviewDate = new Date(
-              item.reviewDates[0],
-            ).toLocaleString();
+        {isLoading && <p>Loading...</p>}
 
-            return (
-              <Sheet key={item.id}>
-                <SheetTrigger className="flex-grow w-full text-left md:w-auto hover:cursor-pointer">
-                  <li>
-                    <Card>
-                      <CardHeader>
-                        <CardTitle>{item.mainText}</CardTitle>
-                      </CardHeader>
-                    </Card>
-                  </li>
-                </SheetTrigger>
-                <SheetContent
-                  side={side}
-                  className="md:w-full max-h-[100vh] overflow-auto"
-                >
-                  <section className="flex flex-col justify-between">
-                    <header>
-                      <SheetTitle asChild>
-                        <h2 className="pb-2 text-3xl font-semibold tracking-tight border-b scroll-m-20 first:mt-0">
-                          {item.mainText}{" "}
-                          <span className="text-gray-400">(#{item.id})</span>
-                        </h2>
-                      </SheetTitle>
+        {sortedItems?.length === 0 && <p>No items to review.</p>}
 
-                      <div className="my-4">
-                        <p className="leading-7 [&:not(:first-child)]:mt-6">
-                          {parsingSubtext(item.subText)}
-                        </p>
-                        <p className="leading-7 [&:not(:first-child)]:mt-6">
-                          Next review at{" "}
-                          <Badge style={randomColor()}>{nextReviewDate}</Badge>
-                        </p>
+        {sortedItems?.map((item) => {
+          const reviewDates = item.reviewDates.map((date) => new Date(date));
+          const nextReviewDate = new Date(item.reviewDates[0]).toLocaleString();
+
+          return (
+            <Sheet key={item.id}>
+              <SheetTrigger className="flex-grow w-full text-left md:w-auto hover:cursor-pointer">
+                <li>
+                  <Card className="transition-all hover:bg-gray-300">
+                    <CardHeader>
+                      <CardTitle>{item.mainText}</CardTitle>
+                    </CardHeader>
+                  </Card>
+                </li>
+              </SheetTrigger>
+              <SheetContent
+                side={side}
+                className="md:w-full max-h-[100vh] overflow-auto"
+              >
+                <section className="flex flex-col justify-between">
+                  <header>
+                    <SheetTitle asChild>
+                      <h2 className="pb-2 text-3xl font-semibold tracking-tight border-b scroll-m-20 first:mt-0">
+                        {item.mainText}{" "}
+                        <span className="text-gray-400">(#{item.id})</span>
+                      </h2>
+                    </SheetTitle>
+
+                    <div className="my-4">
+                      <p className="leading-7 [&:not(:first-child)]:mt-6">
+                        {parsingSubtext(item.subText)}
+                      </p>
+                      <p className="leading-7 [&:not(:first-child)]:mt-6">
+                        Next review at{" "}
+                        <Badge style={randomColor()}>{nextReviewDate}</Badge>
+                      </p>
+                    </div>
+                  </header>
+
+                  <section>
+                    <div className="flex flex-col items-center my-4">
+                      <h4 className="text-xl font-semibold tracking-tight text-black scroll-m-20">
+                        Review Dates
+                      </h4>
+
+                      <div className="text-center">
+                        <Calendar selected={reviewDates} />
                       </div>
-                    </header>
-
-                    <section>
-                      <div className="flex flex-col items-center my-4">
-                        <h4 className="text-xl font-semibold tracking-tight text-black scroll-m-20">
-                          Review Dates
-                        </h4>
-
-                        <div className="text-center">
-                          <Calendar selected={reviewDates} />
-                        </div>
-                      </div>
-                    </section>
-
-                    <footer>
-                      <div className="flex flex-col w-full gap-y-4">
-                        <Button onClick={() => markAsMemorized(item.id)}>
-                          Memorized
-                        </Button>
-                        <Button
-                          onClick={() => remindMeAgainLater(item.id)}
-                          variant="secondary"
-                        >
-                          Remind me again later
-                        </Button>
-                        <Button
-                          onClick={() => deleteItem(item.id)}
-                          variant="destructive"
-                        >
-                          Delete
-                        </Button>
-                      </div>
-                    </footer>
+                    </div>
                   </section>
-                </SheetContent>
-              </Sheet>
-            );
-          })
-        )}
+
+                  <footer>
+                    <div className="flex flex-col w-full gap-y-4">
+                      <Button onClick={() => markAsMemorized(item.id)}>
+                        Memorized
+                      </Button>
+                      <Button
+                        onClick={() => remindMeAgainLater(item.id)}
+                        variant="secondary"
+                      >
+                        Remind me tomorrow
+                      </Button>
+                      <Button
+                        onClick={() => deleteItem(item.id)}
+                        variant="destructive"
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                  </footer>
+                </section>
+              </SheetContent>
+            </Sheet>
+          );
+        })}
       </ul>
     </section>
   );
