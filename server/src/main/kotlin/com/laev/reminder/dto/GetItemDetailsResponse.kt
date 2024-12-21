0e5647e4b03419e363156e6f50c6e8a86f829076
Lyla <mjuudev@gmail.com>
2024-12-21 18:08:42 -0500

    feat: add more item details

--- server/src/main/kotlin/com/laev/reminder/dto/GetItemDetailsResponse.kt
+++ server/src/main/kotlin/com/laev/reminder/dto/GetItemDetailsResponse.kt
@@ -1,9 +1,22 @@
 package com.laev.reminder.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.OffsetDateTime
 
 class GetItemDetailsResponse(
+    val id: Long,
+
+    @Schema(example = "Banana")
+    val mainText: String,
+
+    @Schema(example = "바나나", nullable = true)
+    val subText: String?,
+
+    @Schema(description = "Flag indicating if the item is exposed repeatedly", example = "true")
+    @get:JsonProperty("isRecurring")
+    val isRecurring: Boolean,
+
     @Schema(description = "Dates to review, including today")
     val upcomingReviewDates: List<OffsetDateTime>,
 
