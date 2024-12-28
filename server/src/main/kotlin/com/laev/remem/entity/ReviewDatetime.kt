c77682654aa485797f715504e10572c5e514e07e
Lyla <mjuudev@gmail.com>
2024-12-28 18:59:48 -0500

    chore: add columns in ReviewDatetime entity

--- server/src/main/kotlin/com/laev/remem/entity/ReviewDatetime.kt
+++ server/src/main/kotlin/com/laev/remem/entity/ReviewDatetime.kt
@@ -13,7 +13,16 @@ class ReviewDatetime(
     val start: OffsetDateTime,
 
     @Column(nullable = false)
-    val end: OffsetDateTime,
+    val end: OffsetDateTime = start.plusDays(1),
+
+    @Column(nullable = true)
+    val isMemorized: Boolean? = null,
+
+    @Column(nullable = true)
+    val isSkipped: Boolean? = null,
+
+    @Column(nullable = false)
+    val isDeleted: Boolean = false,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id", nullable = false)
